721b135421498ad6a8e582219451348af43952cf
Hugo <hugo@blackthorn.io>
2022-05-16 09:45:58 -0300

    Update app

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('It Works!')
+  res.send('It Works and update!')
 });
 
 app.listen(process.env.PORT || 3200);
