e0c211165de3a48c367704ccf10d7c95577b1e08
Hugo <hugo@blackthorn.io>
2022-05-26 10:43:41 -0300

    small changes

--- .github/nameCheck.sh
+++ .github/nameCheck.sh
@@ -1,6 +1,5 @@
 branch_original_name=$1  #recieve name parameter
-branchPrefix=$(echo $branch_original_name | cut -d '/' -f 1)
 branchName=$(echo $branch_original_name | cut -d '/' -f 2)  #get branchName without feature/ string
 branchNameUntil30Chars=${branchName:0:30} #trim name
-heroku_name=$branchPrefix"/bt-events-"$branchNameUntil30Chars
+heroku_name="bt-events-"$branchNameUntil30Chars
 echo "CUSTOM_NAME=$heroku_name" >> $GITHUB_ENV
